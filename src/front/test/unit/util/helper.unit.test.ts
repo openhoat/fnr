0ee75df31b73757fcbe9e6eeecfa3d116185ff82
Olivier Penhoat <openhoat@gmail.com>
2023-09-18 10:29:32 +0200

    [TEST] Add helper front unit test

---
+++ src/front/test/unit/util/helper.unit.test.ts
@@ -0,0 +1,25 @@
+import { getBaseUrl } from '../../../main/util/helper'
+
+describe('frontend tests', () => {
+  describe('frontend unit tests', () => {
+    describe('helper', () => {
+      describe('getBaseUrl', () => {
+        test('should return "https://local.io" given location protocol is "https:" and location host is "local.io"', () => {
+          // Given
+          const location = {
+            host: 'local.io',
+            protocol: 'https:',
+          }
+          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+          // @ts-ignore
+          // noinspection JSConstantReassignment,ES6ShorthandObjectProperty
+          global.window = { location }
+          // When
+          const baseUrl = getBaseUrl()
+          // Then
+          expect(baseUrl).toBe('https://local.io')
+        })
+      })
+    })
+  })
+})
