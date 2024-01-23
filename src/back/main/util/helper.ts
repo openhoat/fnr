ef003bb6e35d8a445c2291a7a8a256ed2bcea490
Olivier Penhoat <openhoat@gmail.com>
2024-01-23 18:56:22 +0100

    [BUILD] Add camel case utils

--- src/back/main/util/helper.ts
+++ src/back/main/util/helper.ts
@@ -1,5 +1,25 @@
 import type { FastifyBaseLogger } from 'fastify'
 
+export const toWords = (s: string): RegExpMatchArray | null => {
+  const regex =
+    /[A-Z\u00C0-\u00D6\u00D8-\u00DE]?[a-z\u00DF-\u00F6\u00F8-\u00FF]+|[A-Z\u00C0-\u00D6\u00D8-\u00DE]+(?![a-z\u00DF-\u00F6\u00F8-\u00FF])|\d+/g
+  return s.match(regex)
+}
+
+export const toCamelCase = (s: string): string => {
+  const words = toWords(s)
+  if (!words) {
+    return ''
+  }
+  const camelCaseWords: string[] = words.map((word, index) => {
+    const lowerCaseWord = word.toLowerCase()
+    return index > 0
+      ? `${lowerCaseWord.slice(0, 1).toUpperCase()}${lowerCaseWord.slice(1)}`
+      : lowerCaseWord
+  })
+  return camelCaseWords.join('')
+}
+
 export const isKey = <T extends object>(o: T, k: PropertyKey): k is keyof T => {
   return k in o
 }
