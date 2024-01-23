5872d267cdf6adba9bd9d24163b3f51d1434ec9c
Olivier Penhoat <openhoat@gmail.com>
2024-01-23 18:56:38 +0100

    [REFACTOR] Improve error handling

--- src/back/main/util/error.handler.ts
+++ src/back/main/util/error.handler.ts
@@ -1,36 +1,70 @@
 import type { Boom } from '@hapi/boom'
 import { html } from 'common-tags'
-import type { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify'
+import type {
+  FastifyError,
+  FastifyInstance,
+  FastifyReply,
+  FastifyRequest,
+} from 'fastify'
 import HttpStatusCodes from 'http-status-codes'
 
-const isBoomType = (err: Boom | Error): err is Boom => {
+type SupportedError = Boom | Error | FastifyError
+
+interface ErrorResponse {
+  error: string
+  message: string
+  statusCode: number
+}
+
+const isBoomError = (err: SupportedError): err is Boom => {
   const boomError = err as { isBoom?: boolean }
   return !!boomError.isBoom
 }
 
+const isFastifyError = (err: SupportedError): err is FastifyError => {
+  const fastifyError = err as Partial<FastifyError>
+  return (
+    typeof fastifyError.code === 'string' &&
+    typeof fastifyError.name === 'string' &&
+    typeof fastifyError.statusCode === 'number'
+  )
+}
+
+const getBoomErrorResponse = (
+  err: SupportedError,
+): Partial<ErrorResponse> | undefined =>
+  isBoomError(err)
+    ? {
+        error: err.output.payload.error,
+        message: err.output.payload.message,
+        statusCode: err.output.payload.statusCode,
+      }
+    : undefined
+
+const getFastifyErrorResponse = (
+  err: SupportedError,
+): Partial<ErrorResponse> | undefined =>
+  isFastifyError(err)
+    ? {
+        error: err.code,
+        message: err.message,
+        statusCode: err.statusCode,
+      }
+    : undefined
+
 const errorHandler = function (
   this: FastifyInstance,
-  err: Boom | Error,
+  err: Boom | Error | FastifyError,
   request: FastifyRequest,
   reply: FastifyReply,
 ): string | { error: string; message: string; statusCode: number } {
   this.log.trace(err)
-  let statusCode
-  let error
-  let message
-  if (isBoomType(err)) {
-    statusCode = err.output.payload.statusCode
-    error = err.output.payload.error
-    message = err.output.payload.message
-  }
-  if (statusCode === undefined) {
-    statusCode = HttpStatusCodes.INTERNAL_SERVER_ERROR
-  }
-  if (error === undefined) {
-    error = err.name || 'Internal Error'
-  }
-  if (message === undefined) {
-    message = err.message || 'Unknown error'
+  const { error, message, statusCode }: ErrorResponse = {
+    error: err.name || 'Internal Error',
+    message: err.message || 'Unknown error',
+    statusCode: HttpStatusCodes.INTERNAL_SERVER_ERROR,
+    ...getFastifyErrorResponse(err),
+    ...getBoomErrorResponse(err),
   }
   this.log.error(
     `Error: ${JSON.stringify({
