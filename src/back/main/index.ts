705e5663d7a02d248d2fb3e22330209baf65116d
Olivier Penhoat <openhoat@gmail.com>
2024-01-29 17:52:17 +0100

    [REFACTOR] Improve bootstrap script

--- src/back/main/index.ts
+++ src/back/main/index.ts
@@ -1,19 +1,22 @@
-import type { FastifyInstance } from 'fastify'
-
 import server from './server'
 
 const bootstrap = async (): Promise<void> => {
-  let fastify: FastifyInstance | undefined
   try {
-    fastify = server.init()
+    const fastify = server.init()
+    const { log } = fastify
+    log.debug('Register process "SIGINT" signal handling')
+    process.once('SIGINT', (signal): void => {
+      log.debug(`Detected process signal: ${signal}`)
+      void server.stop(fastify).then(() => {
+        log.info('Nothing more to do: exiting…')
+        // eslint-disable-next-line unicorn/no-process-exit
+        process.exit(0)
+      })
+    })
     await server.configure(fastify)
     await server.start(fastify)
   } catch (error) {
-    if (fastify) {
-      fastify.log.error(error)
-    } else {
-      console.error(error)
-    }
+    console.error(error)
     // eslint-disable-next-line unicorn/no-process-exit
     process.exit(1)
   }
