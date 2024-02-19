272ec4099fa381b3fd146cffffde3b8129e8e866
Olivier Penhoat <openhoat@gmail.com>
2024-02-19 15:10:07 +0100

    [FEAT] Register config and sign-in|out routes

--- src/frontend/main/App.tsx
+++ src/frontend/main/App.tsx
@@ -3,9 +3,13 @@ import './index.css'
 import type { FC } from 'react'
 import { Route, Routes } from 'react-router-dom'
 
+import { ProtectedRoutes } from './hooks/ProtectedRoutes'
 import { AppLayout } from './layouts/AppLayout'
 import { AboutPage } from './pages/AboutPage'
+import { ConfigPage } from './pages/ConfigPage'
 import { HomePage } from './pages/HomePage'
+import { SignInPage } from './pages/SignInPage'
+import { SignOutPage } from './pages/SignOutPage'
 
 const App: FC = () => (
   <>
@@ -13,6 +17,11 @@ const App: FC = () => (
       <Route path="/app" element={<AppLayout />}>
         <Route path="" element={<HomePage />} />
         <Route path="about" element={<AboutPage />} />
+        <Route path="sign-in" element={<SignInPage />} />
+        <Route element={<ProtectedRoutes />}>
+          <Route path="config" element={<ConfigPage />} />
+          <Route path="sign-out" element={<SignOutPage />} />
+        </Route>
       </Route>
     </Routes>
   </>
