22b1764927fb282220678ba6f66e7fe406a06418
Olivier Penhoat <openhoat@gmail.com>
2024-02-19 15:04:28 +0100

    [FEAT] Add authentication to Swagger plugin

--- src/backend/main/interfaces/http/fastify/plugins/swagger-ui.plugin.ts
+++ src/backend/main/interfaces/http/fastify/plugins/swagger-ui.plugin.ts
@@ -1,13 +1,33 @@
 import { join } from 'node:path'
 
+import type { FastifyBasicAuthOptions } from '@fastify/basic-auth'
+import fastifyBasicAuth from '@fastify/basic-auth'
 import fastifySwagger from '@fastify/swagger'
 import fastifySwaggerUi from '@fastify/swagger-ui'
+import Boom from '@hapi/boom'
 import type { FastifyPluginAsync } from 'fastify'
 
 const swaggerUiPlugin: FastifyPluginAsync = async (fastify) => {
   const { config } = fastify.iocContainer
-  const { baseDir } = config
+  const { baseDir, authUsername, authPassword } = config
   const swaggerBaseDir = join(baseDir, 'swagger')
+  const validate = authUsername
+    ? (username: string, password: string): Promise<void> => {
+        if (username !== authUsername || password !== authPassword) {
+          throw Boom.unauthorized()
+        }
+        return Promise.resolve()
+      }
+    : undefined
+  if (validate) {
+    const basicAuthOptions: FastifyBasicAuthOptions = {
+      validate,
+      authenticate: { realm: 'Swagger console' },
+    }
+    void fastify.register(fastifyBasicAuth, basicAuthOptions)
+    await fastify.after()
+    fastify.addHook('onRequest', fastify.basicAuth)
+  }
   await fastify.register(fastifySwagger, {
     mode: 'static',
     specification: {
