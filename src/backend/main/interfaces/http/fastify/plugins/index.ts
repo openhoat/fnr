119741bb40e4d5f3c8109e27b780124b43e34028
Olivier Penhoat <openhoat@gmail.com>
2024-02-19 15:04:43 +0100

    [FEAT] Register JWT plugin

--- src/backend/main/interfaces/http/fastify/plugins/index.ts
+++ src/backend/main/interfaces/http/fastify/plugins/index.ts
@@ -1,4 +1,5 @@
 import fastifyAccepts from '@fastify/accepts'
+import fastifyCookie from '@fastify/cookie'
 import fastifyCors from '@fastify/cors'
 import fastifyFormbody from '@fastify/formbody'
 import type { FastifyInstance, FastifyPluginAsync } from 'fastify'
@@ -8,6 +9,7 @@ import fastifyPlugin from 'fastify-plugin'
 import { registerPlugin } from '../util/fastify-plugin.registerer'
 import { apidocPlugin } from './apidoc.plugin'
 import { homePagePlugin } from './home-page.plugin'
+import { jwtPlugin } from './jwt.plugin'
 import { swaggerUiPlugin } from './swagger-ui.plugin'
 
 const plugins: FastifyPluginAsync = fastifyPlugin(
@@ -20,10 +22,16 @@ const plugins: FastifyPluginAsync = fastifyPlugin(
     })
     await registerPlugin(fastify, 'formBodyParser', fastifyFormbody)
     await registerPlugin(fastify, 'accepts', fastifyAccepts)
+    await registerPlugin(fastify, 'cookie', fastifyCookie, {
+      secret: config.cookieSecret,
+    })
     if (config.corsOrigin) {
       await registerPlugin(fastify, 'cors', fastifyCors, {
         origin: config.corsOrigin,
       })
+    }
+    if (config.jwtSecret) {
+      await registerPlugin(fastify, 'jwt', jwtPlugin)
     }
     await registerPlugin(fastify, 'apidoc', apidocPlugin)
     await registerPlugin(fastify, 'swaggerUi', swaggerUiPlugin)
