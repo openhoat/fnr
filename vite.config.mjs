0ae12853ad5131814886108e225013026c8d4290
Olivier Penhoat <openhoat@gmail.com>
2024-02-20 10:15:36 +0100

    [BUILD] Update typescript config to include common sources

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,19 +1,23 @@
-import { dirname, relative, resolve } from 'node:path'
+import { dirname, resolve } from 'node:path'
 import { fileURLToPath } from 'node:url'
 
 import viteReact from '@vitejs/plugin-react-swc'
-import { defineConfig } from 'vite'
+import { defineConfig, splitVendorChunkPlugin } from 'vite'
 
 const baseDir = dirname(fileURLToPath(new URL(import.meta.url)))
+const outDir = resolve(baseDir, 'dist', 'client')
 const rootDir = resolve(baseDir, 'src', 'frontend', 'main')
 
 /** @type {import('vite').PluginOption} */
-const plugins = [viteReact({ jsxRuntime: 'classic' })]
+const plugins = [viteReact(/*{ jsxRuntime: 'classic' }*/), splitVendorChunkPlugin()]
 
 /** @type {import('vite').BuildOptions} */
 const build = {
+  outDir,
   chunkSizeWarningLimit: 1024 * 2,
-  outDir: relative(rootDir, resolve(baseDir, 'dist', 'client')),
+  rollupOptions: {
+    input: 'src/frontend/main/index.html',
+  },
   sourcemap: 'inline',
 }
 
