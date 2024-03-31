openhoat/template-node-react-ts

[FEAT] Add backend sources

src/backend/main/application/ioc/inversify/inversify-ioc-container.ts
@@ -1,0 +1,53 @@
+import { Container } from 'inversify'
+
+import { PinoLogger } from '../../../infra/logger/pino/pino-logger'
+import { FastifyHttpServer } from '../../../interfaces/http/fastify/fastify-http-server'
+import type { Config } from '../../../types/application/config'
+import type { IocContainer } from '../../../types/application/ioc'
+import type { HttpServer } from '../../../types/interfaces/http/server'
+import type { Logger } from '../../../types/util/logger'
+import { recordToString } from '../../../util/helper'
+import { IOC_TYPES } from '../ioc-types'
+
+class InversifyIocContainer implements IocContainer {
+  readonly #container: Container
+
+  get config(): Config {
+    return this.#container.get(IOC_TYPES.Config)
+  }
+
+  get httpServer(): HttpServer {
+    return this.#container.get(IOC_TYPES.HttpServer)
+  }
+
+  get logger(): Logger {
+    return this.#container.get(IOC_TYPES.Logger)
+  }
+
+  constructor(config: Config) {
+    this.#container = new Container()
+    const logger = new PinoLogger(config)
+    logger.debug(`Loaded config:\n\t${recordToString(config)}`)
+    logger.debug('Initializing IoC container…')
+    this.#registerConfig(config)
+    this.#registerLogger(logger)
+    this.#registerHttpServer(logger)
+    logger.info('IoC container initialized.')
+  }
+
+  #registerConfig(config: Config): void {
+    this.#container.bind(IOC_TYPES.Config).toConstantValue(config)
+  }
+
+  #registerHttpServer(logger: PinoLogger): void {
+    this.#container
+      .bind(IOC_TYPES.HttpServer)
+      .toConstantValue(new FastifyHttpServer(this, logger.pinoLogger))
+  }
+
+  #registerLogger(logger: Logger): void {
+    this.#container.bind(IOC_TYPES.Logger).toConstantValue(logger)
+  }
+}
+
+export { InversifyIocContainer }

src/backend/main/interfaces/http/fastify/errors/normalizers/fastify.error.normalizer.ts
@@ -1,0 +1,28 @@
+import type { FastifyError } from 'fastify'
+
+import type {
+  ErrorNormalizer,
+  ErrorResponse,
+} from '../../../../../types/interfaces/http/fastify/errors'
+
+const isFastifyError = (error: unknown): error is FastifyError => {
+  const fastifyError = error as Partial<FastifyError>
+  return (
+    typeof fastifyError.code === 'string' &&
+    typeof fastifyError.name === 'string' &&
+    typeof fastifyError.statusCode === 'number'
+  )
+}
+
+const fastifyErrorNormalizer: ErrorNormalizer = (
+  error: unknown,
+): Partial<ErrorResponse> | undefined =>
+  isFastifyError(error)
+    ? {
+        error: error.code,
+        message: error.message,
+        statusCode: error.statusCode,
+      }
+    : undefined
+
+export { fastifyErrorNormalizer, isFastifyError }

openhoat/fnr

src/backend/main/application/ioc/inversify/inversify-ioc-container.ts
@@ -1,0 +1,53 @@
+import { Container } from 'inversify'
+
+import { AppLogger } from '../../../infra/logger/app-logger'
+import { FastifyHttpServer } from '../../../interfaces/http/fastify/fastify-http-server'
+import type { Config } from '../../../types/application/config'
+import type { IocContainer } from '../../../types/application/ioc'
+import type { HttpServer } from '../../../types/interfaces/http/server'
+import type { Logger } from '../../../types/util/logger'
+import { recordToString } from '../../../util/helper'
+import { IOC_TYPES } from '../ioc-types'
+
+class InversifyIocContainer implements IocContainer {
+  readonly #container: Container
+
+  get config(): Config {
+    return this.#container.get(IOC_TYPES.Config)
+  }
+
+  get httpServer(): HttpServer {
+    return this.#container.get(IOC_TYPES.HttpServer)
+  }
+
+  get logger(): Logger {
+    return this.#container.get(IOC_TYPES.Logger)
+  }
+
+  constructor(config: Config) {
+    this.#container = new Container()
+    const logger = new AppLogger(config)
+    logger.debug(`Loaded config:\n\t${recordToString(config)}`)
+    logger.debug('Initializing IoC container…')
+    this.#registerConfig(config)
+    this.#registerLogger(logger)
+    this.#registerHttpServer(logger)
+    logger.info('IoC container initialized.')
+  }
+
+  #registerConfig(config: Config): void {
+    this.#container.bind(IOC_TYPES.Config).toConstantValue(config)
+  }
+
+  #registerHttpServer(logger: AppLogger): void {
+    this.#container
+      .bind(IOC_TYPES.HttpServer)
+      .toConstantValue(new FastifyHttpServer(this, logger.logger))
+  }
+
+  #registerLogger(logger: Logger): void {
+    this.#container.bind(IOC_TYPES.Logger).toConstantValue(logger)
+  }
+}
+
+export { InversifyIocContainer }

src/backend/main/interfaces/http/fastify/errors/normalizers/fastify.error.normalizer.ts
@@ -1,0 +1,28 @@
+import type { FastifyError } from 'fastify'
+
+import type {
+  ErrorNormalizer,
+  ErrorResponse,
+} from '../../../../../types/interfaces/http/fastify/errors'
+
+const isFastifyError = (error: unknown): error is FastifyError => {
+  const fastifyError = error as Partial<FastifyError>
+  return (
+    typeof fastifyError.code === 'string' &&
+    typeof fastifyError.message === 'string' &&
+    typeof fastifyError.statusCode === 'number'
+  )
+}
+
+const fastifyErrorNormalizer: ErrorNormalizer = (
+  error: unknown,
+): Partial<ErrorResponse> | undefined =>
+  isFastifyError(error)
+    ? {
+        error: error.code,
+        message: error.message,
+        statusCode: error.statusCode,
+      }
+    : undefined
+
+export { fastifyErrorNormalizer, isFastifyError }